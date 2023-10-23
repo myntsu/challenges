e1ead318b08d76ece700b6d3c7575cc3abecb604
Myntsu <dingodille16@hotmail.com>
2023-10-23 18:03:36 -0300

    Updating JS

--- achievement-tracker/script.js
+++ achievement-tracker/script.js
@@ -1,36 +1,59 @@
 document.querySelector("#copyright").textContent = new Date().getFullYear().toString();
 
-document.addEventListener("click", toggleDropdown);
-document.addEventListener("keydown", e => {
-  if (e.key === "Enter" || e.key === " ") {
-    toggleDropdown(e);
+document.addEventListener("click", function(e) {
+  const button = e.target.closest("[data-dropdown-button]");
+  let currentDropdown;
+
+  if (button) {
+    currentDropdown = button.parentElement;
+    toggleDropdown(currentDropdown);
+  } else if (!e.target.closest("[data-dropdown]")) {
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+      dropdown.classList.remove("active");
+      const dropdownButton = dropdown.querySelector("[data-dropdown-button]");
+      if (dropdownButton) {
+        dropdownButton.setAttribute("aria-expanded", false);
+        const anchors = dropdown.querySelectorAll(".dropdown a");
+        anchors.forEach(anchor => {
+          anchor.setAttribute("aria-hidden", "true");
+          anchor.setAttribute("tabindex", "-1");
+        });
+      }
+    });
   }
 });
 
-function toggleDropdown(e) {
-  const button = e.target.closest("[data-dropdown-button]");
-  if (!button && e.target.closest("[data-dropdown]") != null) return;
+function toggleDropdown(dropdown) {
+  const button = dropdown.querySelector("[data-dropdown-button]");
+  const isActive = dropdown.classList.toggle("active");
 
-  let currentDropdown;
-  if (button) {
-    currentDropdown = e.target.closest("[data-dropdown]");
-    if (currentDropdown) {
-      const isActive = currentDropdown.classList.toggle("active");
-      button.setAttribute("aria-expanded", isActive);
-      if (isActive) {
-        const firstItem = currentDropdown.querySelector("a");
-        if (firstItem) firstItem.focus();
-      } else {
-        button.focus();
-      }
-    }
+  button.setAttribute("aria-expanded", isActive);
+  const anchors = dropdown.querySelectorAll(".dropdown a");
+
+  if (isActive) {
+    anchors.forEach(anchor => {
+      anchor.setAttribute("aria-hidden", "false");
+      anchor.setAttribute("tabindex", "0");
+    });
+    const firstItem = anchors[0];
+    if (firstItem) firstItem.focus();
+  } else {
+    anchors.forEach(anchor => {
+      anchor.setAttribute("aria-hidden", "true");
+      anchor.setAttribute("tabindex", "-1");
+    });
+    button.focus();
   }
-
-  document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-    if (dropdown === currentDropdown) return;
-    dropdown.classList.remove("active");
-    const dropdownButton = dropdown.closest("[data-dropdown-button]");
-    if (dropdownButton) dropdownButton.setAttribute("aria-expanded", false);
-  });
 }
 
+// Get the button and nav elements
+const hamburger = document.querySelector('.hamburger-menu');
+const nav = document.querySelector('nav');
+
+// Attach click event to the button
+hamburger.addEventListener('click', () => {
+    // Toggle aria-expanded attribute and visible class
+    const isExpanded = nav.getAttribute('aria-expanded') === 'true';
+    nav.setAttribute('aria-expanded', !isExpanded);
+    nav.classList.toggle('visible');
+});
