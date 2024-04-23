0159669d7da4470400e947e898713bc0013a21e6
Myntsu <dingodille16@hotmail.com>
2024-04-22 20:44:35 -0400

    removing uneccesary js

--- moon-jelly/script.js
+++ moon-jelly/script.js
@@ -23,29 +23,6 @@ document.addEventListener("click", function(e) {
   }
 });
 
-function toggleDropdown(dropdown) {
-  const button = dropdown.querySelector("[data-dropdown-button]");
-  const isActive = dropdown.classList.toggle("active");
-
-  button.setAttribute("aria-expanded", isActive);
-  const anchors = dropdown.querySelectorAll(".dropdown a");
-
-  if (isActive) {
-    anchors.forEach(anchor => {
-      anchor.setAttribute("aria-hidden", "false");
-      anchor.setAttribute("tabindex", "0");
-    });
-    const firstItem = anchors[0];
-    if (firstItem) firstItem.focus();
-  } else {
-    anchors.forEach(anchor => {
-      anchor.setAttribute("aria-hidden", "true");
-      anchor.setAttribute("tabindex", "-1");
-    });
-    button.focus();
-  }
-}
-
 const hamburger = document.querySelector('.hamburger-menu');
 const nav = document.querySelector('nav');
 
@@ -53,27 +30,4 @@ hamburger.addEventListener('click', () => {
     const isExpanded = nav.getAttribute('aria-expanded') === 'true';
     nav.setAttribute('aria-expanded', !isExpanded);
     nav.classList.toggle('visible');
-});
-
-let tags = document.querySelectorAll('.tag-list .tag');
-
-tags.forEach(tag => {
-  let dataTag = tag.getAttribute('data-tag');
-  let textContent = tag.textContent.trim();
-
-  switch(dataTag) {
-    case 'version':
-      let versionNumber = textContent.replace('V', '');
-      tag.setAttribute('title', `Version ${versionNumber}`);
-      break;
-    case 'krystal':
-      let krystalNumber = textContent.replace('Krystal', '').trim();
-      tag.setAttribute('title', `${krystalNumber} krystals`);
-      break;
-    case 'type':
-      if(tag.hasAttribute('data-tag-hidden')) {
-        tag.setAttribute('title', 'This is a secret achievement');
-      }
-      break;
-  }
 });
