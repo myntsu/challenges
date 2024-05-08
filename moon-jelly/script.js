3bef63b2c967f794a0e44c14324ecec64ae68dc9
Myntsu <dingodille16@hotmail.com>
2024-05-07 20:36:51 -0400

    updating js

--- moon-jelly/script.js
+++ moon-jelly/script.js
@@ -1,40 +1,24 @@
+// auto copyright
 document.querySelector(
   "#copyright"
 ).textContent = new Date().getFullYear().toString();
 
-document.addEventListener("click", function (e) {
-  const button = e.target.closest("[data-dropdown-button]");
-  let currentDropdown;
-
-  if (button) {
-    currentDropdown = button.parentElement;
-    toggleDropdown(currentDropdown);
-  } else if (!e.target.closest("[data-dropdown]")) {
-    document.querySelectorAll("[data-dropdown].active").forEach((dropdown) => {
-      dropdown.classList.remove("active");
-      const dropdownButton = dropdown.querySelector("[data-dropdown-button]");
-      if (dropdownButton) {
-        dropdownButton.setAttribute("aria-expanded", false);
-        const anchors = dropdown.querySelectorAll(".dropdown a");
-        anchors.forEach((anchor) => {
-          anchor.setAttribute("aria-hidden", "true");
-          anchor.setAttribute("tabindex", "-1");
-        });
-      }
-    });
-  }
-});
-
+// hamb menu open/close
 const hamburger = document.querySelector(".hamburger-menu");
 const nav = document.querySelector("nav");
 
 hamburger.addEventListener("click", () => {
-  const isExpanded = nav.getAttribute("aria-expanded") === "true";
-  nav.setAttribute("aria-expanded", !isExpanded);
-  nav.classList.toggle("visible");
+  const isVisible = nav.classList.contains("visible");
+  if (!isVisible) {
+    nav.classList.add("visible");
+    nav.setAttribute("aria-expanded", false);
+  } else {
+    nav.classList.remove("visible");
+    nav.setAttribute("aria-expanded", true);
+  }
 });
 
-// Settings
+// settings menu
 const settingsDialog = document.querySelector("#settingsDialog");
 const confirmDialog = document.querySelector("#confirmDialog");
 const openButton = document.querySelector("#openSettings");
@@ -103,6 +87,7 @@ window.addEventListener("click", function (event) {
   }
 });
 
+// darkmode check
 function applySettings(settings) {
   document.documentElement.style.setProperty(
     "--background",
