3156e227d7d2187c7a23a64b5fdbeda2bef2b76b
Myntsu <dingodille16@hotmail.com>
2024-09-03 12:08:58 -0400

    some vue practice

---
+++ vue/vue-router/vue-router/src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/Contact.vue')
+    },
+    {
+      path: '/form',
+      name: 'form',
+      component: () => import('../views/Form.vue')
+    }
+  ]
+})
+
+export default router
---
+++ vue/vue-router/vue-router/src/views/Form.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="form">
+        <h1>This is a form page</h1>
+        <form action="" method="get">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required />
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required />
+
+            <label for="message">Message:</label>
+            <textarea id="message" name="message" required></textarea>
+
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+    .form {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+    }
+
+    form {
+        display: grid;
+        gap: .25rem;
+    }
+}
+</style>
