b512549342c7727c1595e30e25147d25c8b7ab6a
Myntsu <dingodille16@hotmail.com>
2024-09-13 11:55:24 -0300

    adding firebase script

---
+++ vue/vue-with-firebase/src/components/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCpmXpAbmCv0ZmshhAjhj36lsTj5yVZLY",
+  authDomain: "vue-with-firebase-ab96f.firebaseapp.com",
+  projectId: "vue-with-firebase-ab96f",
+  storageBucket: "vue-with-firebase-ab96f.appspot.com",
+  messagingSenderId: "131822431677",
+  appId: "1:131822431677:web:18c62eb06cd173db15f6d4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app 
