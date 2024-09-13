e05924442f3d7a9df3d6c78e4f9357acec2a6a74
Myntsu <dingodille16@hotmail.com>
2024-09-13 12:14:05 -0300

    adding firebase db call

---
+++ vue/vue-with-firebase/src/components/FirebaseComponent.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>Chilean Products</h1>
+        <ul>
+            <li v-for="product in products">
+                {{ product.name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import { getFirestore, collection, getDocs } from 'firebase/firestore';
+    import firebaseApp from '../firebaseConfig';
+
+    export default {
+        name: 'FirebaseComponent',
+        data() {
+            products: [],
+        },
+
+        async mounted () {
+            try {
+                const db = getFirestore(firebaseApp);
+                const productsSnapshot = await getDocs(collection(db, 'products'));
+                productsSnapshot.forEach((doc) => {
+                    this.products.push(doc.data());
+                });
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
+</script>
