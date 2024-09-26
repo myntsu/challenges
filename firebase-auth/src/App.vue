ce11731509bd5ca1c792c959802c59387805e2fd
Myntsu <dingodille16@hotmail.com>
2024-09-26 13:55:35 -0300

    login in and out

--- firebase-auth/src/App.vue
+++ firebase-auth/src/App.vue
@@ -1,12 +1,39 @@
 <template>
   <div id="app">
     <router-link to="/">Home</router-link>
-    <router-link to="/registro">Registro</router-link>
+    <router-link v-if="isLoggedIn" to="/secret">Secret</router-link>
+    <router-link v-if="!isLoggedIn" to="/registro">Registro</router-link>
+    <router-link v-if="!isLoggedIn" to="/login">Login</router-link>
+    <button v-if="isLoggedIn" @click="logout">Cerrar sesión</button>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import { auth, signOut, onAuthStateChanged } from './auth.js';
+
+export default {
+  
+  data() {
+    return {
+      isLoggedIn: false
+    }
+  },
+
+  created() {
+    onAuthStateChanged(auth, (user) => {
+      this.isLoggedIn = !!user;
+    })
+  },
+
+  methods: {
+    async logout() {
+      await signOut(auth);
+      this.$router.push('/login');
+    }
+  }
+}
+
 </script>
 
 <style scoped>
--- firebase-auth/src/router/index.js
+++ firebase-auth/src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SecretView from '@/views/SecretView.vue'
 import RegistroView from '@/views/RegistroView.vue'
 import LoginView from '@/views/LoginView.vue'
 
+import { getAuth } from 'firebase/auth';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,6 +13,12 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/secret',
+      name: 'secret',
+      component: SecretView,
+      meta: {requiresAuth: true}
     },
     {
       path: '/registro',
@@ -23,6 +31,17 @@ const router = createRouter({
       component: LoginView
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  const auth = getAuth();
+  const user = auth.currentUser;
+
+  if (to.meta.requiresAuth &&!user) {
+    next('/login');
+  } else {
+    next();
+  }
 })
 
 export default router
