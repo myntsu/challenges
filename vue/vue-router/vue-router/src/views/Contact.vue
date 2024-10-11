3bb81d52c83e8c7386992c318fa85ccfcdd4b113
Myntsu <dingodille16@hotmail.com>
2024-10-10 23:10:56 -0300

    sometimes i hate vue

--- vue/vue-router/vue-router/src/views/Contact.vue
+++ vue/vue-router/vue-router/src/views/Contact.vue
@@ -12,11 +12,4 @@
         align-items: center;
     }
 }
-
-.remembering {
-    text-shadow: 2px 2px 0 black;
-    box-shadow: 2px 2px 2px 2px black;
-    filter: drop-shadow(2px 2px black);
-    backdrop-filter: drop-shadow(2px 2px black);
-}
 </style>
